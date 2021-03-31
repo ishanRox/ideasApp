48e3bd4483435c7be40200656b0c1a8e8af27be7
ishanRox <roxishan8@gmail.com>
2021-03-31 12:39:30 +0530

    typeorm config added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { Logger } from '@nestjs/common';
 
 const port = process.env.PORT || 8080;
 
@@ -8,5 +9,6 @@ const port = process.env.PORT || 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
+  Logger.log(`server running on port-${port} url- http://localhost:${port}`,'Bootstrap');
 }
 bootstrap();
